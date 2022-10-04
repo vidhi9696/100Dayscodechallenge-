5672500b04eb6c0a4bc658b0cb561b5cb950d0d1
vidhi9696 <114758784+vidhi9696@users.noreply.github.com>
2022-10-04 21:47:53 +0530

    Create java Stdin and Stdout.java
    In this challenge, you must read an integer, a double, and a String from stdin, then print the values according to the instructions in the Output Format section below. To make the problem a little easier, a portion of the code is provided for you in the editor.
    
    Note: We recommend completing Java Stdin and Stdout I before attempting this challenge.
    
    Sample Input
    
    42
    3.1415
    Welcome to HackerRank's Java tutorials!

---
+++ DAY4/java Stdin and Stdout.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner yo = new Scanner(System.in);
+        int i = yo.nextInt();
+        double d = yo.nextDouble();
+         yo.nextLine();
+        String s = yo.nextLine();
+         
+        // Write your code here.
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+} 
