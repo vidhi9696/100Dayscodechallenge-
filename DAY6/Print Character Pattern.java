fd8c5911fe6c9d72c438701802ef928b569c13ec
vidhi9696 <114758784+vidhi9696@users.noreply.github.com>
2022-10-07 23:04:37 +0530

    Update Print Character Pattern.java

--- DAY6/Print Character Pattern.java
+++ DAY6/Print Character Pattern.java
@@ -1,10 +1,13 @@
 public class Main
 {
     public static void main(String[] args) {
+        
         int n=4;
         //taking 2nd alphabet as a starting pattern
         char ch='B';
+        //outer loop
         for(int i =1;i<=n;i++){
+            //Inner loop 
             for(int chars=1;chars<=i;chars++){
                 System.out.print(ch);
                 ch++;
