938213903f83fdc99e2a14c18bb06ad24eaa60cd
vidhi9696 <114758784+vidhi9696@users.noreply.github.com>
2022-10-07 23:49:07 +0530

    Update Half_Pyramid Pattern.java

--- DAY7/Half_Pyramid Pattern.java
+++ DAY7/Half_Pyramid Pattern.java
@@ -1,7 +1,7 @@
 public class Main
 {
     public static void main(String[] args) {
-        //System.out.println("Hello World");
+       
         for(int i=1;i<=6;i++)
         {
             for(int j=1;j<=i;j++)
