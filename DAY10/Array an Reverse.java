7b41d449f31222fde46b6045bde40c6d33e005a8
vidhi9696 <114758784+vidhi9696@users.noreply.github.com>
2022-10-27 00:15:54 +0530

    Update Array an Reverse.java

--- DAY10/Array an Reverse.java
+++ DAY10/Array an Reverse.java
@@ -1,1 +1,24 @@
+import java.util.*;
+//Reverse an array
+public class Main {
+    public static void reverse(int numbers[]) {
+        int first = 0, last = numbers.length - 1;
+        while (first < last) {
+            //swap
+            int temp = numbers[last];
+            numbers[last] = numbers[first];
+            numbers[first] = temp;
+            first++;
+            last--;
+        }
+    }
+
+    public static void main(String arg[]) {
+        int numbers[] = {2, 4, 6, 8, 10};
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[1] + "");
+        }
+        System.out.println();
+    }
+}
 
