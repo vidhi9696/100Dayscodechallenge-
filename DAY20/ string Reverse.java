f545adfc57251035a6df1d312fec8d4df7014778
vidhi9696 <114758784+vidhi9696@users.noreply.github.com>
2022-11-07 22:46:46 +0530

    Update and rename java string reverse.java to  string Reverse.java

---
+++ DAY20/ string Reverse.java
@@ -0,0 +1,23 @@
+question
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        String reverse = new StringBuilder(A).reverse().toString();
+    if (A.equals(reverse)) {
+      System.out.println("Yes");
+    }
+    else {
+      System.out.println("No");
+    }
+  }
+
+        
+    }
+
