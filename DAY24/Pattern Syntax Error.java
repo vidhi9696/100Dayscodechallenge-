670c7a3018c507430e39d13351abbc37f78bcb5a
vidhi9696 <114758784+vidhi9696@users.noreply.github.com>
2022-11-10 22:22:25 +0530

    pattern syntax error

---
+++ DAY24/Pattern Syntax Error.java
@@ -0,0 +1,29 @@
+Question :https://www.hackerrank.com/challenges/pattern-syntax-checker/problem?isFullScreen=true
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class Solution
+{
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		int testCases = Integer.parseInt(in.nextLine());
+		while(testCases>0){
+			String pattern = in.nextLine();
+          	//Write your 
+              try {
+                Pattern.compile(pattern);
+                System.out.println("Valid");
+            } catch (PatternSyntaxException e) {
+                System.out.println("Invalid");
+            }
+            testCases--;
+        }
+    }
+
+
+		
+	}
+    
+
+
+
