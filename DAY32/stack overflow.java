c2387260463a0d8a23b8d8a7dcb4672a40fea3f1
vidhi9696 <114758784+vidhi9696@users.noreply.github.com>
2022-11-21 23:03:04 +0530

    Update stack overflow.java

--- DAY32/stack overflow.java
+++ DAY32/stack overflow.java
@@ -1,1 +1,16 @@
 
+public class RecursionBasic {
+   public static void printDec(int n ) {
+    //if (n==1) {
+        //System.out.println();
+       // return;
+
+    //}
+    System.out.println(n+" ");
+    printDec(n-1);
+}
+public static void main (String args[]) {
+    int n= 10;
+    printDec(n);
+}
+}
