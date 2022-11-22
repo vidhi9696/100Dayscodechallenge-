9994902a63fd6c685f6f439b789901b6d9da54b8
vidhi9696 <114758784+vidhi9696@users.noreply.github.com>
2022-11-22 23:53:46 +0530

    Java Map

---
+++ DAY35/java Map.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+import java.io.*;
+
+class Solution{
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        
+        Map<String,String> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            String name = scanner.nextLine();
+            String phone = scanner.nextLine();
+            map.put(name, phone);
+        }
+        while (scanner.hasNextLine()) {
+            String query = scanner.nextLine();
+            if (map.containsKey(query)) {
+                System.out.println(query + "=" + map.get(query));
+            } else {
+                System.out.println("Not found");
+            }
+        }
+    }
+}
