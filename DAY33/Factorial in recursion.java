74c0e02d46b04b6a2384cc404d9d1ec129104926
vidhi9696 <114758784+vidhi9696@users.noreply.github.com>
2022-11-23 00:28:53 +0530

    factorial of n number in recursion

--- DAY33/Factorial in recursion.java
+++ DAY33/Factorial in recursion.java
@@ -1,1 +1,33 @@
 
+public class recursion {
+    public static void printDec(int n) {
+        if (n == 1) {
+            System.out.println(n);
+            return;
+        }
+        System.out.println(n+" ");
+        printDec(n-1);
+    }
+    public static void println(int n){
+        printDec(n-1);
+        System.out.println(n+" ");
+
+    }
+public static int fact(int n) {
+    if(n == 0){
+        return 1;
+
+    }
+    int fn = n * fact(n-1);
+    return fn;
+
+}
+public static void main(String args[]){
+    int n = 5;
+   System.out.println(fact(n));
+
+
+}
+
+
+}
